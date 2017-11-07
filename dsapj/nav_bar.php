6db91c38dcd2b6486bf605ae2e025f1b3ba9577e
Anmol Middha <anmolmiddha2827@gmail.com>
2017-11-07 21:07:47 +0530

    huffman compression

---
+++ dsapj/nav_bar.php
@@ -0,0 +1,21 @@
+<?php echo '<nav class="navbar navbar-inverse ">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="">DS Project</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+  
+      <ul class="nav navbar-nav navbar-right">
+		<li><a href="huffman_compression.php">Compression</a></li>
+		<li><a href="huffman_decompression.php">Decompression</a></li>
+        <li><a href="huffman_conversion.php">Huffman Conversion</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>';
+?>
